71ca6ccedb6b8646bcde16225b3261092afdbd02
ismailkavak <poyrazkvk@hotmail.com>
2025-06-20 12:20:23 +0300

    Created and connected database

--- src/main/java/com/example/CloudStorage/CloudStorageApplication.java
+++ src/main/java/com/example/CloudStorage/CloudStorageApplication.java
@@ -2,12 +2,12 @@ package com.example.CloudStorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CloudStorageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
+
 }
